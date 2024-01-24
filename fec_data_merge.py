cdf3d67d27e2217dade42a6febf8f733138fd274
Michael Nolan <manolan@uw.edu>
2024-01-23 23:45:10 -0500

    Enh: add committee names to overlap reports, enables file appending to preexisting committee csv files. ISSUE: find way to deal with rate limits (1000/hr, maybe just a debugging problem)

--- fec_data_merge.py
+++ fec_data_merge.py
@@ -1,8 +1,10 @@
 import numpy as np
+import os
 import pandas as pd
 
 from argparse import ArgumentParser, BooleanOptionalAction
 from datetime import datetime
+from glob import glob
 from itertools import chain, combinations
 
 from open_fec_scraper import DEFAULT_COMMITTEES
@@ -19,28 +21,36 @@ def powerset(iterable,min_size=0):
     return chain.from_iterable(combinations(s, r) for r in range(min_size,len(s)+1))
 
 def load_committee_data(committee_id):
-    file_path = f'./data/receipt_{committee_id}.csv'
-    return pd.read_csv(file_path,index_col=0,low_memory=False)
+    committee_file_list = glob(f'./data/receipt_{committee_id}_*.csv')
+    if committee_file_list:
+        file_path = committee_file_list[0]
+        committee_name = os.path.basename(file_path).split('_')[-1][:-4]
+        return pd.read_csv(file_path,index_col=0,low_memory=False), committee_name
+    else:
+        # no file for that committee found. Skipping it this time
+        return None, None
+    
 
 def init_log_file():
-    log_file_path = f"./data/receipt_overlap_ \
-        {datetime.strftime(datetime.now(),'%d-%m-%y_%H-%M-%S')}.md"
-    # Path(log_file_path).touch()
+    log_file_path = f"./data/receipt_overlap_{datetime.strftime(datetime.now(),'%d-%m-%y_%H-%M-%S')}.md"
     return log_file_path
 
-def log_contributor_overlap(log_file,committee_ids,shared_contributors):
+def log_contributor_overlap(log_file,committee_ids,committee_names,shared_contributors):
     with open(log_file,mode='a') as log_f:
         print(f"# {', '.join(committee_ids)}:",file=log_f)
+        print(f"{', '.join(committee_names)}",file=log_f)
         print(f'shared contributors found:\t{len(shared_contributors)}',file=log_f)
         [print(f'- {s_c}',file=log_f) for s_c in shared_contributors]
         print('\n',file=log_f)
 
-def find_contributor_overlap(receipt_data_list,committee_ids,log_file=None,min_size=2):
+def find_contributor_overlap(receipt_data_list,committee_ids,committee_name_list,log_file=None,min_size=2):
     for subset_idx in powerset(np.arange(len(receipt_data_list)),min_size=2):
         subset_idx = list(subset_idx)
         _receipt_data_list = list(np.array(receipt_data_list,dtype=object)[subset_idx])
         _committee_ids = list(np.array(committee_ids)[subset_idx])
+        _committee_names = list(np.array(committee_name_list)[subset_idx])
         print(_committee_ids)
+        print(_committee_names)
         shared_contributors = list(
             set.intersection(
                 *[set(r_d['contributor_name']) for r_d in _receipt_data_list]
@@ -48,16 +58,23 @@ def find_contributor_overlap(receipt_data_list,committee_ids,log_file=None,min_s
         )
         print(f'shared contributors found:\t{len(shared_contributors)}')
         if log_file:
-            log_contributor_overlap(log_file,_committee_ids,shared_contributors)
+            log_contributor_overlap(log_file,_committee_ids,_committee_names,shared_contributors)
 
 def main(committee_ids,log_results):
     if committee_ids:
         pass
     else:
         committee_ids = DEFAULT_COMMITTEES
-    receipt_data_list = [load_committee_data(c_id) for c_id in committee_ids]
+    receipt_data_list = []
+    committee_name_list = []
+    for c_id in committee_ids:
+        _receipt_data, _committee_name = load_committee_data(c_id)
+        if _receipt_data is None:
+            continue
+        receipt_data_list.append(_receipt_data)
+        committee_name_list.append(_committee_name)
     log_file = init_log_file() if log_results else None
-    find_contributor_overlap(receipt_data_list,committee_ids,log_file)
+    find_contributor_overlap(receipt_data_list,committee_ids,committee_name_list,log_file)
 
 if __name__ == "__main__":
     args = parse_inputs()
--- open_fec_scraper.py
+++ open_fec_scraper.py
@@ -4,6 +4,8 @@ import requests as req
 
 from argparse import ArgumentParser
 from dotenv import load_dotenv
+from glob import glob
+from json.decoder import JSONDecodeError
 from tqdm import tqdm
 
 DEFAULT_COMMITTEES = [
@@ -12,6 +14,7 @@ DEFAULT_COMMITTEES = [
     'C00806307',    # B. Williams
     'C00765719',    # M. De La Cruz
     'C00801985',    # K. Kiley
+    'C00726687',    # P. Junge
 ] #default committees
 
 def parse_inputs():
@@ -26,35 +29,68 @@ def configure_data_directory():
     if not os.path.exists('./data'):
         os.makedirs('./data')
 
-def get_receipt_data(committee_ids):
+def get_new_receipt_data(committee_ids):
     for committee_id in committee_ids:
-        print(f'committee_id:\t{committee_id}')
-        committee_receipt_df = get_committee_recipt_df(committee_id)
-        save_receipt_df(committee_receipt_df,committee_id)
+        try:
+            print(f'committee_id:\t{committee_id}')
+            existing_df_file_list = glob(f'./data/receipt_{committee_id}_*.csv')
+            if existing_df_file_list:
+                existing_df = pd.read_csv(existing_df_file_list[0],low_memory=False)
+                min_date = existing_df['contribution_receipt_date'][0]
+                _committee_name = os.path.basename(existing_df_file_list[0]).split('_')[-1][:-4]
+            else:
+                existing_df = None
+                min_date = None
+            committee_receipt_df, committee_name = get_committee_recipt_df(committee_id,min_date)
+            if committee_name is None:
+                committee_name = _committee_name
+            committee_receipt_df = pd.concat([committee_receipt_df,existing_df],ignore_index=True)
+            save_receipt_df(committee_receipt_df,committee_id,committee_name)
+        except JSONDecodeError as jde:
+            # not sure what causes this error, seems to be somewhere between
+            # the builting JSON decoding package and the way the data comes
+            # back from the server.
+            print(jde)
+
+def api_get(url):
+    r = req.get(url)
+    if r.status_code == 200:
+        return r
+    else:
+        print(f'ERROR: Status Code {r.status_code} ({r.reason})')
+        r.raise_for_status()
 
 def api_return_to_df(receipt_r):
     try:
-        receipt_df = pd.DataFrame.from_records(
-            receipt_r.json()['results'],
-            exclude=['committee','contributor','contributor_name']
-        )
+        if receipt_r.json()['results']:
+            receipt_df = pd.DataFrame.from_records(
+                receipt_r.json()['results'],
+                exclude=['committee','contributor','contributor_name']
+            )
+            contributor_name = [r['contributor_name'].replace(',',';') for r in receipt_r.json()['results']]
+            receipt_df['contributor_name'] = contributor_name
+        return receipt_df
     except:
         breakpoint()
-    contributor_name = [r['contributor_name'].replace(',',';') for r in receipt_r.json()['results']]
-    receipt_df['contributor_name'] = contributor_name
-    return receipt_df
 
-def get_committee_recipt_df(committee_id):
-    receipt_api_call_url = create_receipt_api_call_url(committee_id)
-    receipt_r = req.get(receipt_api_call_url)
-    if receipt_r.status_code == 200:
+def get_committee_recipt_df(committee_id,min_date):
+    receipt_api_call_url = create_receipt_api_call_url(
+        committee_id,
+        pagination_dict={},
+        min_date=min_date
+    )
+    receipt_r = api_get(receipt_api_call_url)
+    if receipt_r.json()['results']:
+        committee_name = receipt_r.json()['results'][0]['committee']['name'].replace(' ','-')
+        print(committee_name)
         receipt_df = api_return_to_df(receipt_r)
         for _ in tqdm(range(1,receipt_r.json()['pagination']['pages'])):
             receipt_api_call_url = create_receipt_api_call_url(
                 committee_id,
-                pagination_dict=receipt_r.json()['pagination']['last_indexes']
+                pagination_dict=receipt_r.json()['pagination']['last_indexes'],
+                min_date=min_date
             )
-            receipt_r = req.get(receipt_api_call_url)
+            receipt_r = api_get(receipt_api_call_url)
             receipt_df = pd.concat(
                 [
                     receipt_df,
@@ -62,16 +98,17 @@ def get_committee_recipt_df(committee_id):
                 ],
                 ignore_index=True
             )
-        return receipt_df
     else:
-        raise(req.HTTPError(receipt_api_call_url,receipt_r))
+        receipt_df = pd.DataFrame([])
+        committee_name = None
+    return receipt_df, committee_name
 
-def save_receipt_df(receipt_df,committee_id):
-    csv_file_name = f'./data/receipt_{committee_id}.csv'
+def save_receipt_df(receipt_df,committee_id,committee_name):
+    csv_file_name = f'./data/receipt_{committee_id}_{committee_name}.csv'
     print(f'saving recipt data to:\t{csv_file_name}')
     receipt_df.to_csv(csv_file_name)
 
-def create_receipt_api_call_url(committee_id,pagination_dict={},per_page=100):
+def create_receipt_api_call_url(committee_id,pagination_dict={},min_date=None,per_page=100):
     api_url_root = 'https://api.open.fec.gov/v1/schedules/schedule_a/?'
     request_dict = {
         'committee_id': committee_id, #input
@@ -79,6 +116,8 @@ def create_receipt_api_call_url(committee_id,pagination_dict={},per_page=100):
         'api_key': os.getenv("OPENFEC_API_KEY"),
         **pagination_dict,
     }
+    if min_date:
+        request_dict['min_date'] = min_date
     return api_url_root + '&'.join([f'{k}={v}' for k, v in request_dict.items()])
 
 def main(committee_ids):
@@ -88,7 +127,7 @@ def main(committee_ids):
         committee_ids = DEFAULT_COMMITTEES
     load_dotenv()
     configure_data_directory()
-    get_receipt_data(committee_ids)
+    get_new_receipt_data(committee_ids)
 
 if __name__ == "__main__":
     args = parse_inputs()
