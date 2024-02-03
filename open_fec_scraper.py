983c35ce97f477f55aa0c00d12a5b76cde47d75c
Michael Nolan <manolan@uw.edu>
2024-02-03 14:22:11 -0500

    Enh: refactor receipt, disbursement downloading code into a single function

--- open_fec_scraper.py
+++ open_fec_scraper.py
@@ -23,40 +23,25 @@ def configure_data_directory():
     if not os.path.exists('./data'):
         os.makedirs('./data')
 
-def get_new_receipt_data(committee_id):
-    schedule = 'a'
-    existing_df_file_list = glob(f'./data/receipt_{committee_id}_*.csv')
+def get_new_schedule_data(id,schedule):
+    schedule_name, date_key = {
+        'a': ('receipt','contribution_receipt_date'),
+        'b': ('disbursement','disbursement_date'),
+        'e': ('ind-exp','expenditure_date'),
+    }.get(schedule)
+    existing_df_file_list = glob(f'./data/{schedule_name}_{id}_*.csv')
     if existing_df_file_list:
         existing_df = pd.read_csv(existing_df_file_list[0],low_memory=False)
-        min_date = existing_df['contribution_receipt_date'][0]
-        _committee_name = os.path.basename(existing_df_file_list[0]).split('_')[-1][:-4]
+        min_date = existing_df[date_key][0]
+        _name = os.path.basename(existing_df_file_list[0]).split('_')[-1][:-4]
     else:
         existing_df = None
         min_date = None
-    committee_receipt_df, committee_name = get_findisc_df(committee_id,min_date,schedule)
-    if committee_name is None:
-        committee_name = _committee_name
-    committee_receipt_df = pd.concat([committee_receipt_df,existing_df],ignore_index=True)
-    save_findisc_df(committee_receipt_df,committee_id,committee_name,schedule)
-
-def get_new_disbursement_data(committee_id):
-    schedule = 'b'
-    existing_df_file_list = glob(f'./data/disbursement_{committee_id}_*.csv')
-    if existing_df_file_list:
-        existing_df = pd.read_csv(existing_df_file_list[0],low_memory=False)
-        min_date = existing_df['disbursement_date'][0]
-        _committee_name = os.path.basename(existing_df_file_list[0]).split('_')[-1][:-4]
-    else:
-        existing_df = None
-        min_date = None
-    committee_disbursement_df, committee_name = get_findisc_df(committee_id,min_date,schedule)
-    if committee_name is None:
-        committee_name = _committee_name
-    committee_disbursement_df = pd.concat([committee_disbursement_df,existing_df],ignore_index=True)
-    save_findisc_df(committee_disbursement_df,committee_id,committee_name,schedule)
-
-def get_new_ind_exp_data(candidate_id):
-    pass
+    data_df, name = get_findisc_df(id,min_date,schedule)
+    if name is None:
+        name = _name
+    data_df = pd.concat([data_df,existing_df],ignore_index=True)
+    save_findisc_df(data_df,id,name,schedule_name)
 
 def api_return_to_df(r):
     exclude_list = list(set.intersection(
@@ -75,20 +60,16 @@ def api_return_to_df(r):
     return df
 
 def get_findisc_df(id,min_date,schedule):
-    create_api_call = {
-        'a': create_receipt_api_call_url,
-        'b': create_disbursement_api_call_url,
-        'e': create_ind_exp_api_call_url,
-    }.get(schedule)
-    api_call_url = create_api_call(id,pagination_dict={},min_date=min_date)
+    api_call_url = create_api_call_url(id,schedule,pagination_dict={},min_date=min_date)
     r = api_get(api_call_url)
     if r.json()['results']:
         name = r.json()['results'][0]['committee']['name'].replace(' ','-')
         print(name)
         df = api_return_to_df(r)
         for _ in tqdm(range(1,r.json()['pagination']['pages'])):
-            api_call_url = create_api_call(
+            api_call_url = create_api_call_url(
                 id,
+                schedule,
                 pagination_dict=r.json()['pagination']['last_indexes'],
                 min_date=min_date
             )
@@ -99,17 +80,12 @@ def get_findisc_df(id,min_date,schedule):
         name = None
     return df, name
 
-def save_findisc_df(df,id,name,schedule):
-    schedule_name = {
-        'a': 'receipt',
-        'b': 'disbursement',
-        'e': 'ind-exp',
-    }.get(schedule)
+def save_findisc_df(df,id,name,schedule_name):
     csv_file_name = f'./data/{schedule_name}_{id}_{name}.csv'
     print(f'saving data to:\t{csv_file_name}')
     df.to_csv(csv_file_name)
 
-def api_request_dict(pagination_dict,min_date,**kwargs):
+def api_request_str_from_params(api_url_root='',pagination_dict='',min_date=None,**kwargs):
     request_dict = {
         **kwargs,
         'api_key': os.getenv("OPENFEC_API_KEY"),
@@ -117,26 +93,15 @@ def api_request_dict(pagination_dict,min_date,**kwargs):
     }
     if min_date:
         request_dict['min_date'] = min_date
-    return request_dict
-
-def api_str_from_dict(api_url_root,request_dict):
     return api_url_root + '&'.join([f'{k}={v}' for k, v in request_dict.items()])
 
-#TODO: these functions should be one function? the candidate_id/committee_id in kwargs gums up the works
-def create_receipt_api_call_url(committee_id,pagination_dict={},min_date=None,per_page=100):
-    api_url_root = RECEIPT_API_URL_ROOT
-    request_dict = api_request_dict(pagination_dict,min_date,committee_id=committee_id,per_page=per_page)
-    return api_str_from_dict(api_url_root,request_dict)
-
-def create_disbursement_api_call_url(committee_id,pagination_dict={},min_date=None,per_page=100):
-    api_url_root = DISBURSEMENT_API_URL_ROOT
-    request_dict = api_request_dict(pagination_dict,min_date,committee_id=committee_id,per_page=per_page)
-    return api_str_from_dict(api_url_root,request_dict)
-
-def create_ind_exp_api_call_url(candidate_id,pagination_dict={},min_date=None,per_page=100):
-    api_url_root = IND_EXP_API_URL_ROOT
-    request_dict = api_request_dict(pagination_dict,min_date,candidate_id=candidate_id,per_page=per_page)
-    return api_str_from_dict(api_url_root,request_dict)
+def create_api_call_url(id,schedule,pagination_dict={},min_date=None,per_page=100):
+    param_dict = {
+        'a': {'api_url_root': RECEIPT_API_URL_ROOT,'committee_id': id},
+        'b': {'api_url_root': DISBURSEMENT_API_URL_ROOT,'committee_id': id},
+        'e': {'api_url_root': IND_EXP_API_URL_ROOT,'candidate_id': id},
+    }.get(schedule)
+    return api_request_str_from_params(pagination_dict=pagination_dict,min_date=min_date,per_page=per_page,**param_dict)
 
 def main():
     candidate_data_list = DEFAULT_CANDIDATE_DATA
@@ -144,9 +109,9 @@ def main():
     configure_data_directory()
     for candidate_id, committee_id in candidate_data_list:
         print(f'committee_id:\t{committee_id}')
-        get_new_receipt_data(committee_id)
-        get_new_disbursement_data(committee_id)
-        get_new_ind_exp_data(candidate_id)
+        get_new_schedule_data(committee_id,'a')
+        get_new_schedule_data(committee_id,'b')
+        # get_new_schedule_data(candidate_id,'e') #TODO: build the schedule e api string, work it into your framework.
 
 if __name__ == "__main__":
     args = parse_inputs()
